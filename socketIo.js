b045681a40e5c78457b7f638f2e43627657813f8
ysRedhouse <yongsin0129@gmail.com>
2022-08-03 08:44:03 +0800

    refactor : using userId to get Database info  for socketIo

--- socketIo.js
+++ socketIo.js
@@ -3,6 +3,7 @@ const http = require('http')
 const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server)
+const { User } = require('./models')
 
 // 記錄之前的訊息
 const historyLogs = [
@@ -14,26 +15,25 @@ const historyLogs = [
 
 const chattingUsers = {}
 
-// io 建立連線
+// --------------    io --------------    建立連線
 io.on('connection', socket => {
   // 建立連線的用戶都可以看到以前的訊息
   historyLogs.forEach(log => {
     socket.emit('chat message', log)
   })
 
-  // 有使用者上線
-  socket.on('login', data => {
-    console.log('a user ' + data.userName + ' connected')
+  // --------------   有使用者上線
+  socket.on('login', async loginData => {
+    // 透過資料庫找尋使用者資料
+    const loginUser = await User.findByPk(loginData.userId, { raw: true })
     // saving userId to object with socket ID
-    chattingUsers[socket.id] = {
-      name: data.userName,
-      avatar: data.avatar
-    }
-    io.sockets.emit('broadcast', { description: data.userName + ' 上線了 !!' })
-    // io.emit('chat message', data.userName + ' 上線了 !!')
+    chattingUsers[socket.id] = loginUser
+    // 在前後端都留下訊息
+    console.log('a user ' + chattingUsers[socket.id].name + ' connected')
+    io.sockets.emit('broadcast', { description: chattingUsers[socket.id].name + ' 上線了 !!' })
   })
 
-  // 有使用者離線
+  // --------------   有使用者離線
   socket.on('disconnect', () => {
     console.log('user ' + chattingUsers[socket.id]?.name + ' disconnected')
     io.sockets.emit('broadcast', { description: chattingUsers[socket.id]?.name + ' 離線了 !!' })
@@ -41,14 +41,14 @@ io.on('connection', socket => {
     delete chattingUsers[socket.id]
   })
 
-  // 監聽 chat message emit 的任何訊息
-  socket.on('chat message', msg => {
+  // --------------   監聽 --------------   chat message emit 的任何訊息
+  socket.on('chat message', receivedMsg => {
     const returnObj = {
+      id: chattingUsers[socket.id].id,
       name: chattingUsers[socket.id].name,
       avatar: chattingUsers[socket.id].avatar,
-      message: msg
+      message: receivedMsg
     }
-    console.log('returnObj: ' + returnObj)
     // 將 訊息丟回到 socket chat message
     io.emit('chat message', returnObj)
   })
