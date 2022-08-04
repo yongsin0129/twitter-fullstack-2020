7193a7a74b12d0fdc87c59101e645410b27b48d6
yongsin0129 <yongsin0129@gmail.com>
2022-08-04 22:30:16 +0800

    feat : create private room for both users

--- socketIo-pm.js
+++ socketIo-pm.js
@@ -1,20 +1,27 @@
 const { io, chattingUsers } = require('./socketIo')
 
 module.exports = socket => {
-  socket.on('private message', ({ receivedMsg, receiver }) => {
+  const loginUserId = chattingUsers[socket.id].id
+
+  socket.on('private message', ({ receivedMsg, targetUserId }) => {
     const returnObj = {
-      id: chattingUsers[socket.id].id,
+      id: loginUserId,
       name: chattingUsers[socket.id].name,
       avatar: chattingUsers[socket.id].avatar,
       message: receivedMsg,
-      to: receiver
+      to: targetUserId
     }
     // 將 訊息丟回到 socket chat message
-    io.to(receiver).emit('private message', returnObj)
+    io.to(`${loginUserId}to${targetUserId}`)
+      .to(`${targetUserId}to${loginUserId}`)
+      .emit('private message', returnObj)
+    console.log("socket.on ~ targetUserId", targetUserId)
+    console.log("socket.on ~ loginUserId", loginUserId)
+    console.log(`${targetUserId}to${loginUserId}`)
   })
 
-  socket.on('join room', room => {
-    socket.join(room)
+  socket.on('join room', targetUserId => {
+    socket.join(`${loginUserId}to${targetUserId}`)
   })
 
   socket.on('checkPM', userId => {
