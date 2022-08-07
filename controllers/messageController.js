71e0fb051815278ed3590275f8fe271c3f63e4c3
gene.31985906@gmail.com <gene.31985906@gmail.com>
2022-08-07 15:39:38 +0800

    feat:add right notice bar && love icon hover

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,3 +1,6 @@
+const { User, Tweet, Reply, Like } = require('../models')
+const { getUser } = require('../_helpers')
+const bcrypt = require('bcryptjs')
 const messageController = {
   getAllChatroom: async (req, res, next) => {
     try {
@@ -16,11 +19,23 @@ const messageController = {
     }
   },
   getNotices: async (req, res, next) => {
-    try {
-      res.render('notice')
-    } catch (err) {
-      next(err)
-    }
+    const observedUserId = req.params.id
+    const loginUser = getUser(req)
+
+    return User.findByPk(observedUserId, {
+      nest: true,
+      include: [Tweet, { model: User, as: 'Followings' }]
+    })
+      .then(user => {
+        const result = user.Followings.map(user => {
+          return {
+            ...user.toJSON(),
+            isFollowed: loginUser?.Followings.some(f => f.id === user.id)
+          }
+        }).sort((a, b) => b.Followship.createdAt - a.Followship.createdAt)
+        res.render('notice', { observedUser: user.toJSON(), followings: result })
+      })
+      .catch(err => next(err))
   },
   postSubscribe: async (req, res, next) => {
     try {
